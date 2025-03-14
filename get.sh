c6e0566f5ba62a00ed87410aef532a4611765835
techspice0 <mibr0813@gmail.com>
2025-03-14 00:13:08 -0400

    Add files via upload

---
+++ get.sh
@@ -0,0 +1,15 @@
+# Rob the internet of SSHRD_Script
+git clone https://github.com/verygenericname/SSHRD_Script.git
+
+# Make the folder easier to type
+mv ./SSHRD_Script ./sshrd_script
+
+# Send Legacy-IOS-Kit into the abyss (your device)
+git clone https://github.com/LukeZGD/Legacy-iOS-Kit.git
+
+# Make folder easier to type
+
+mv ./Legacy-iOS-Kit ./legacy-ios-kit
+
+# Exit
+exit
